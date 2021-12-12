557601530a3b19b15d774a6cd80b3cae624ba336
Umesha-Nirmani <58287574+Umesha-Nirmani@users.noreply.github.com>
2021-12-12 17:43:03 +0530

    Add project files.

---
+++ ContactBook/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace ContactBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFName = new System.Windows.Forms.Label();
+            this.labelLName = new System.Windows.Forms.Label();
+            this.labelContact = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.textBoxFName = new System.Windows.Forms.TextBox();
+            this.textBoxLName = new System.Windows.Forms.TextBox();
+            this.textBoxContact = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelFName
+            // 
+            this.labelFName.AutoSize = true;
+            this.labelFName.Location = new System.Drawing.Point(85, 68);
+            this.labelFName.Name = "labelFName";
+            this.labelFName.Size = new System.Drawing.Size(93, 21);
+            this.labelFName.TabIndex = 0;
+            this.labelFName.Text = "First Name";
+            // 
+            // labelLName
+            // 
+            this.labelLName.AutoSize = true;
+            this.labelLName.Location = new System.Drawing.Point(85, 107);
+            this.labelLName.Name = "labelLName";
+            this.labelLName.Size = new System.Drawing.Size(92, 21);
+            this.labelLName.TabIndex = 1;
+            this.labelLName.Text = "Last Name";
+            // 
+            // labelContact
+            // 
+            this.labelContact.AutoSize = true;
+            this.labelContact.Location = new System.Drawing.Point(85, 145);
+            this.labelContact.Name = "labelContact";
+            this.labelContact.Size = new System.Drawing.Size(68, 21);
+            this.labelContact.TabIndex = 2;
+            this.labelContact.Text = "Contact";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(85, 184);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(51, 21);
+            this.labelEmail.TabIndex = 3;
+            this.labelEmail.Text = "Email";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(85, 225);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(69, 21);
+            this.labelAddress.TabIndex = 4;
+            this.labelAddress.Text = "Address";
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.BackColor = System.Drawing.Color.MidnightBlue;
+            this.buttonCreate.FlatAppearance.BorderSize = 0;
+            this.buttonCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCreate.ForeColor = System.Drawing.Color.White;
+            this.buttonCreate.Location = new System.Drawing.Point(152, 412);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(85, 35);
+            this.buttonCreate.TabIndex = 5;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = false;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.MidnightBlue;
+            this.buttonDelete.FlatAppearance.BorderSize = 0;
+            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDelete.ForeColor = System.Drawing.Color.White;
+            this.buttonDelete.Location = new System.Drawing.Point(257, 412);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(86, 35);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.MidnightBlue;
+            this.buttonCancel.FlatAppearance.BorderSize = 0;
+            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCancel.ForeColor = System.Drawing.Color.White;
+            this.buttonCancel.Location = new System.Drawing.Point(360, 412);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(89, 35);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // textBoxFName
+            // 
+            this.textBoxFName.Location = new System.Drawing.Point(240, 65);
+            this.textBoxFName.Name = "textBoxFName";
+            this.textBoxFName.Size = new System.Drawing.Size(250, 33);
+            this.textBoxFName.TabIndex = 8;
+            // 
+            // textBoxLName
+            // 
+            this.textBoxLName.Location = new System.Drawing.Point(240, 104);
+            this.textBoxLName.Name = "textBoxLName";
+            this.textBoxLName.Size = new System.Drawing.Size(250, 33);
+            this.textBoxLName.TabIndex = 9;
+            // 
+            // textBoxContact
+            // 
+            this.textBoxContact.Location = new System.Drawing.Point(240, 142);
+            this.textBoxContact.Name = "textBoxContact";
+            this.textBoxContact.Size = new System.Drawing.Size(250, 33);
+            this.textBoxContact.TabIndex = 10;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(240, 181);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(250, 33);
+            this.textBoxEmail.TabIndex = 11;
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(240, 222);
+            this.textBoxAddress.Multiline = true;
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(250, 106);
+            this.textBoxAddress.TabIndex = 8;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(568, 119);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowTemplate.Height = 25;
+            this.dataGridView.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.Size = new System.Drawing.Size(546, 421);
+            this.dataGridView.TabIndex = 13;
+            this.dataGridView.DoubleClick += new System.EventHandler(this.dataGridView_DoubleClick);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.MidnightBlue;
+            this.buttonSearch.FlatAppearance.BorderSize = 0;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.ForeColor = System.Drawing.Color.White;
+            this.buttonSearch.Location = new System.Drawing.Point(1025, 65);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(89, 33);
+            this.buttonSearch.TabIndex = 14;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(568, 65);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(441, 33);
+            this.textBoxSearch.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1178, 578);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxContact);
+            this.Controls.Add(this.textBoxLName);
+            this.Controls.Add(this.textBoxFName);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelContact);
+            this.Controls.Add(this.labelLName);
+            this.Controls.Add(this.labelFName);
+            this.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Contact Book";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFName;
+        private System.Windows.Forms.Label labelLName;
+        private System.Windows.Forms.Label labelContact;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox textBoxFName;
+        private System.Windows.Forms.TextBox textBoxLName;
+        private System.Windows.Forms.TextBox textBoxContact;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+    }
+}
+
---
+++ ContactBook/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ContactBook
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=localhost;Database=contactbookdb;Uid=root;Pwd=password;";
+        int contactID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            using(MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("contactAddOrEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_contactID", contactID);
+                mySqlCmd.Parameters.AddWithValue("_firstName", textBoxFName.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_lastName", textBoxLName.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Contact", textBoxContact.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Email", textBoxEmail.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Address", textBoxAddress.Text.Trim());
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Submitted Successfully!");
+                clear();
+                GridFill();
+            }
+        }
+
+        void GridFill()
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlData = new MySqlDataAdapter("contactViewAll", mysqlCon);
+                sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dtblContact = new DataTable();
+                sqlData.Fill(dtblContact);
+                dataGridView.DataSource = dtblContact;
+                dataGridView.Columns[0].Visible = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clear();
+            GridFill();
+        }
+
+        void clear()
+        {
+            textBoxFName.Text = textBoxLName.Text = textBoxContact.Text = textBoxEmail.Text = textBoxAddress.Text = textBoxSearch.Text = "";
+            contactID = 0;
+            buttonCreate.Text = "Create";
+            buttonDelete.Enabled = false;
+        }
+
+        private void dataGridView_DoubleClick(object sender, EventArgs e)
+        {
+           
+            if (dataGridView.CurrentRow.Index != -1)
+            {
+                textBoxFName.Text = dataGridView.CurrentRow.Cells[1].Value.ToString();
+                textBoxLName.Text = dataGridView.CurrentRow.Cells[2].Value.ToString();
+                textBoxContact.Text = dataGridView.CurrentRow.Cells[3].Value.ToString();
+                textBoxEmail.Text = dataGridView.CurrentRow.Cells[4].Value.ToString();
+                textBoxAddress.Text = dataGridView.CurrentRow.Cells[5].Value.ToString();
+                contactID = Convert.ToInt32(dataGridView.CurrentRow.Cells[0].Value.ToString());
+                buttonCreate.Text = "Update";
+                buttonDelete.Enabled = true;
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlData = new MySqlDataAdapter("contactSearchByValue", mysqlCon);
+                sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlData.SelectCommand.Parameters.AddWithValue("_searchValue", textBoxSearch.Text);
+                DataTable dtblContact = new DataTable();
+                sqlData.Fill(dtblContact);
+                dataGridView.DataSource = dtblContact;
+                dataGridView.Columns[0].Visible = false;
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("contactDeleteByID", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_contactID", contactID);
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted Successfully!");
+                clear();
+                GridFill();
+            }
+        }
+    }
+}
